4585ebe3944664b8271b2eeecea15d5d19428fe7
HSNBDES33\eocariz <eocariz@hasar.com>
2018-12-10 16:12:23 -0300

    Uso de propiedad children / composición vs extensión

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,75 +1,14 @@
 import React from 'react';
+import StandardFrame from './StandardFrame';
 import Customers from './Customers';
-import LogoutButton from './LogoutButton';
-import LoginButton from './LoginButton';
-import Greeting from './Greeting';
 
-class MainPage extends React.Component {
 
-    state = {
-        isLoggedIn: false
-    };
-
-    constructor(props) {
-        super(props);
-
-        this.handleLoginClick = this
-            .handleLoginClick
-            .bind(this);
-        this.handleLogoutClick = this
-            .handleLogoutClick
-            .bind(this);
-        this.state = {
-            isLoggedIn: false
-        };
-
-        this.loginButton = React.createRef();
-    }
-
-    handleLoginClick() {
-        this.setState({isLoggedIn: true});
-    }
-
-    handleLogoutClick() {
-        this.setState({isLoggedIn: false});
-    }
-
-    componentDidMount() {
-        this.loginButton.current && this
-            .loginButton
-            .current
-            .focus();
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        this.loginButton.current && this
-            .loginButton
-            .current
-            .focus();
-    }
-
-    render() {
-        const isLoggedIn = this.state.isLoggedIn;
-        let button;
-
-        if (isLoggedIn) {
-            button = <LogoutButton onClick={this.handleLogoutClick}/>;
-        } else {
-            button = <LoginButton ref={this.loginButton} onClick={this.handleLoginClick}/>;
-        }
-
-        const getClassName = isLoggedIn => isLoggedIn
-            ? 'log'
-            : 'unlog';
-
-        return (
-            <div className={getClassName(isLoggedIn)}>
-                <Greeting isLoggedIn={isLoggedIn}/> {button}
-                {isLoggedIn && <Customers data={this.props.data}></Customers>
-}
-            </div>
-        );
-    }
-}
+const MainPage = ({data}) => {
+    return (
+        <StandardFrame>
+            <Customers data={data}></Customers>
+        </StandardFrame>
+    );
+};
 
 export default MainPage;
---
+++ src/components/StandardFrame.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import LogoutButton from './LogoutButton';
+import LoginButton from './LoginButton';
+import Greeting from './Greeting';
+
+class StandardFrame extends React.Component {
+
+    state = {
+        isLoggedIn: false
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.handleLoginClick = this
+            .handleLoginClick
+            .bind(this);
+        this.handleLogoutClick = this
+            .handleLogoutClick
+            .bind(this);
+        this.state = {
+            isLoggedIn: false
+        };
+
+        this.loginButton = React.createRef();
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick() {
+        this.setState({isLoggedIn: false});
+    }
+
+    componentDidMount() {
+        this.loginButton.current && this
+            .loginButton
+            .current
+            .focus();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        this.loginButton.current && this
+            .loginButton
+            .current
+            .focus();
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick}/>;
+        } else {
+            button = <LoginButton ref={this.loginButton} onClick={this.handleLoginClick}/>;
+        }
+
+        const getClassName = isLoggedIn => isLoggedIn
+            ? 'log'
+            : 'unlog';
+
+        return (
+            <div className={getClassName(isLoggedIn)}>
+                <Greeting isLoggedIn={isLoggedIn}/> {button}
+                {isLoggedIn && this.props.children}
+            </div>
+        );
+    }
+}
+
+export default StandardFrame;
