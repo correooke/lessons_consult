b70ce2f5319fa4eabf263369ec7827a66be765c2
HSNBDES33\eocariz <eocariz@hasar.com>
2018-12-11 17:47:26 -0300

    Agrego capacidad de Scroll

--- src/components/CustomerRow.js
+++ src/components/CustomerRow.js
@@ -3,17 +3,17 @@ import MediaQuery from 'react-responsive';
 
 // query="(min-device-width: 1224px)"
 
-const CustomerRow = ({id, name, picture}) => {
+const CustomerRow = ({id, name, picture, onClick}) => {
     return (
         <>
             <MediaQuery minDeviceWidth={500}>
-                <div key={id}>
+                <div key={id} onClick={onClick}>
                     <h2>{name}!</h2>
                     <img className='element-animation' src={picture} alt=""/>
                 </div>
             </MediaQuery>        
             <MediaQuery maxDeviceWidth={499}>
-                <div className='customer-row' key={id}>
+                <div className='customer-row' key={id} onClick={onClick}>
                     <h2>{name}</h2>
                 </div>
             </MediaQuery>        
--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import CustomerRow2 from './CustomerRow2';
+import { scroller, Element } from 'react-scroll';
+import CustomerRow from './CustomerRow';
 
 const place = "https://via.placeholder.com/150/FF0000/FFFFFF?text=";
 
 const base = [
     {
-
         id: 1,
         name: "Emiliano Ocariz",
         picture: `${place}Emiliano`
     }
 ];
 
+
+const scroolType = {
+    duration: 500,
+    delay: 50,
+    smooth: true,
+    offset: -10, // Scrolls to element + 50 pixels down the page
+  };
+
+
 const Customers = ({
     data = base
 }) => {
+    const onClick = id => {
+        debugger;
+        const item = data.filter(d => d.id === id)[0];
+        const toScroll = data[data.indexOf(item) + 1];
+        if (toScroll) {
+            scroller.scrollTo(toScroll.id.toString(), scroolType);
+            console.log(toScroll.id.toString());
+            console.log("id:" + id);
+        } else {
+            scroller.scrollTo(data[0].id.toString());
+        }
+
+    };
     return (
-        <div>
-            {data.map(i => <CustomerRow2 {...i}/>)}
+        <div className='customers-list'>
+            {data.map(i => 
+                <Element key={i.id} name={i.id.toString()}>
+                    <CustomerRow {...i} onClick={ () => onClick(i.id)}/>
+                </Element>)}
         </div>
     );
 };
